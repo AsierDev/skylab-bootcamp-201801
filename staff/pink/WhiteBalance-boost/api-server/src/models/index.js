8aba97744b7046e8282e45739f5cf0bb1264408c
AsierDev <asierp.dev@gmail.com>
2018-06-26 19:34:58 +0200

    corrections

--- staff/pink/WhiteBalance-boost/api-server/src/models/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-const { User, Image, Comment } = require('./schemas')
-
-module.exports = {
-	User: mongoose.model('User', User),
-	Image: mongoose.model('Image', Image),
-	Comment: mongoose.model('Comment', Comment)
-}
